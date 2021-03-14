76a806be07fbeaa7852c9095f8c687a9745208bd
Aerevia <rayan69390@hotmail.fr>
2021-03-14 20:02:58 +0100

    Ajoutez des fichiers projet.

---
+++ Strategy.cs
@@ -0,0 +1,69 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Text.RegularExpressions;
+
+namespace SKryll
+{
+    public class Strategy
+    {
+        public string Title { get; set; }
+        public string Alt { get; set; }
+        public string Fuel { get; set; }
+        public float Profit 
+        {
+            get 
+            {
+                return float.Parse(ProfitString.Remove(0, 2).Replace(" %", "").Replace(".", ","));
+            }
+        } 
+        public string ProfitString { get; set; }
+        public DateTime StartTime { get; set; }
+        public string Pair { get; set; }
+        public string Status { get; set; }
+        public string StartingCapitalString { get; set; }
+        public float StartingCapital 
+        { 
+            get
+            {
+                return float.Parse(Regex.Match(StartingCapitalString.Replace(".", ","), @"[-+]*\d+\,\d+|[-+]*\d+").Value);
+            }
+        }
+        public string CurrentValueString { get; set; }
+        public float CurrentValue
+        {
+            get
+            {
+                return float.Parse(Regex.Match(CurrentValueString.Replace(".", ","), @"[-+]*\d+\,\d+|[-+]*\d+").Value);
+            }
+        }
+
+        public string AccumulatedCoin
+        { 
+            get
+            {
+                return Pair.Remove(0, Pair.IndexOf("/") + 2);
+            }
+        }
+
+        public IWebElement WebElement { get; set; }
+        
+
+        public Strategy()
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return Title + Environment.NewLine +
+                   Status + Environment.NewLine +
+                   Fuel + Environment.NewLine +
+                   Pair + Environment.NewLine +
+                   Profit.ToString() + Environment.NewLine +
+                   StartTime + Environment.NewLine +
+                   StartingCapital + Environment.NewLine +
+                   CurrentValue + Environment.NewLine +
+                   AccumulatedCoin;
+        }
+    }
+}
---
+++ StrategyManager.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using Serilog;
+using Serilog.Core;
+
+namespace SKryll
+{
+    public class StrategyManager
+    {
+        string strategiesUrl = @"https://platform.kryll.io/strategies";
+
+        LoggerConfiguration Log = new LoggerConfiguration();
+        Logger logger;
+
+        public ChromeOptions BrowserOptions { get; set; }
+
+        public ChromeDriver Browser { get; set; }
+
+        public List<Strategy> Strategies { get; set; }
+
+        public StrategyManager(ChromeOptions chromeOptions)
+        {
+            logger = Log.CreateLogger();
+
+            Browser = new ChromeDriver(chromeOptions);
+        }
+
+        public StrategyManager(bool isHeadless = true)
+        {
+            logger = Log.CreateLogger();
+            BrowserOptions = new ChromeOptions();
+
+            BrowserOptions.AddArguments(new List<string>() { isHeadless ? "headless" : "head", "disable-gpu", @"user-data-dir=" + Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + @"\Google\Chrome\User Data"});
+
+            Browser = new ChromeDriver(BrowserOptions);
+
+        }
+
+        public List<Strategy> GetStrategies()
+        {
+            Strategies = new List<Strategy>();
+
+            Browser.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            Browser.Navigate().GoToUrl(strategiesUrl);
+
+            var strategiesDiv = Browser.FindElementsByCssSelector("app-card-strategy-dashboard");
+
+            foreach (var item in strategiesDiv)
+            {
+
+                Strategy strategy = new Strategy
+                {
+                    Title = item.FindElement(By.ClassName("title-text")).Text,
+                    Status = item.FindElement(By.ClassName("card-dashboard-status")).Text,
+                    Fuel = item.FindElement(By.ClassName("battery-krl")).Text,
+                    Pair = item.FindElements(By.ClassName("card-dashboard-pairs"))[0].Text,
+                    ProfitString = item.FindElement(By.ClassName("card-dashboard-profits")).Text,
+                    StartTime = DateTime.Parse(item.FindElements(By.ClassName("card-dashboard-pairs"))[1].Text),
+                    //Alt = item.FindElement(By.ClassName("corner")).GetAttribute("alt"),
+                    StartingCapitalString = item.FindElements(By.ClassName("card-body"))[1].FindElements(By.ClassName("toolbar-col"))[0].Text,
+                    CurrentValueString = item.FindElements(By.ClassName("card-body"))[1].FindElements(By.ClassName("toolbar-col"))[1].Text,
+                    WebElement = item
+                };
+
+                Strategies.Add(strategy);
+            }
+
+            return Strategies;
+        }
+
+        public void StopStrategy(Strategy strategy)
+        {
+            strategy.WebElement.Click();
+            Browser.FindElementByClassName("toolbar-actions").FindElements(By.ClassName("btn"))[1].Click();
+            Browser.FindElementByClassName("dialog-buttons").FindElements(By.ClassName("btn"))[1].Click();
+
+        }
+        public void StopStrategy(string title)
+        {
+            StopStrategy(Strategies.Where(s => s.Title.Equals(title)).First());
+        }
+
+        public void Exit()
+        {
+            Browser.Close();
+            Browser.Quit();
+        }
+
+    }
+}
